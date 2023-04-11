aa8ef53a285614a4dbdd7dca7be6d269bc66512e
RustyPPsH <rafaelsonoki@hotmail.com>
2023-04-11 19:59:56 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            ServidorSocket servidor = new ServidorSocket();
+            servidor.rodarServidor();
+        } catch (Exception e) {
+            System.out.println("Exceção:" + e);
+        }
+    }
+}
